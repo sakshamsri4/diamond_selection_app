a8416949e578e4c782fb50cf8ebb0cef9fb477f0
SakshamMustFin <s.saksham@mufin.co.kr>
2025-03-18 11:27:25 +0530

    Add DiamondModel class to represent diamond attributes

---
+++ lib/data/models/diamond_model.dart
@@ -0,0 +1,37 @@
+class DiamondModel {
+  final String lotId;
+  final double size;
+  final double carat;
+  final String lab;
+  final String shape;
+  final String color;
+  final String clarity;
+  final String cut;
+  final String polish;
+  final String symmetry;
+  final String fluorescence;
+  final double discount;
+  final double perCaratRate;
+  final double finalAmount;
+  final String keyToSymbol;
+  final String labComment;
+
+  DiamondModel({
+    required this.lotId,
+    required this.size,
+    required this.carat,
+    required this.lab,
+    required this.shape,
+    required this.color,
+    required this.clarity,
+    required this.cut,
+    required this.polish,
+    required this.symmetry,
+    required this.fluorescence,
+    required this.discount,
+    required this.perCaratRate,
+    required this.finalAmount,
+    required this.keyToSymbol,
+    required this.labComment,
+  });
+}
